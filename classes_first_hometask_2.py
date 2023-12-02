8daa4e2b9c63682a7057e9510874f3e4d4575933
nina1603 <nina16.antonova@yandex.ru>
2023-12-03 00:38:13 +0300

    Second part of homework

---
+++ classes_first_hometask_2.py
@@ -0,0 +1,20 @@
+from classes_first_hometask_1 import Advert
+
+
+class ColorizeMixin:
+    repr_color_code = 33  # yellow
+
+    def __repr__(self):
+        color_option = f'\033[{self.repr_color_code}m{self.title} | {self.price} ₽'
+        return color_option
+
+class Advert_2(ColorizeMixin, Advert):
+    """
+    new class similar to Advert but printing coloured line with ad name and price
+    """
+
+    def __init__(self, json_obj):
+        super().__init__(json_obj)
+
+    def rename_attr(self, key):
+        return super().rename_attr(key)
